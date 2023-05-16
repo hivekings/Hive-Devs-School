1fc09ad04556d9f82e6b35d62a8223977a5d52bd
fefe4 <mynameisfefe@protonmail.com>
2023-05-16 02:19:03 -0300

    test 5

--- app.js
+++ app.js
@@ -34,9 +34,6 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/build/', 'index.html'));
-});
 
 app.use(
   cors({
@@ -46,6 +43,9 @@ app.use(
 
 app.use('/api/', indexRouter);
 
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '/client/build/', 'index.html'));
+});
 
 
 // const allowedOrigins = ['http://localhost:3000', 'http://localhost:5000'];
