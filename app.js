2ab55f31dacd1b579c844f59614afd35ee9cd416
fefe4 <mynameisfefe@protonmail.com>
2023-05-16 01:47:59 -0300

    test2

--- app.js
+++ app.js
@@ -32,9 +32,11 @@ app.use(cookieParser());
 //   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 // });
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/build/', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, '/client/build/', 'index.html'));
+// });
 
 app.use(
   cors({
